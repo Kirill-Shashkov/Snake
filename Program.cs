9055cf27d154044514e71487d5bd32f3a20da503
Kirill <DarkLirik@list.ru>
2018-12-08 20:20:16 +0300

    point class added

--- Program.cs
+++ Program.cs
@@ -10,8 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("oO");
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = "*";
+            p1.Draw();
+
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
             Console.ReadLine();
+        }
+        static void Draw(int a, int b, char sym) {
+            Console.SetCursorPosition(a, b);
+            Console.Write(sym);
         }
     }
 }
---
+++ point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class point
+    {
+        public int a;
+        public int b;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(a, b);
+            Console.Write(sym);
+
+        }
+    }
+}
